600646cd5d0460c71d09eaf4ea8f7c016754924b
Yves Alter <yves@alter.com>
2013-01-03 22:03:23 +0100

    Javadoc

--- libsonos/src/main/java/ch/ownz/s4m/sonos/xml/RenderingControlEventHandler.java
+++ libsonos/src/main/java/ch/ownz/s4m/sonos/xml/RenderingControlEventHandler.java
@@ -44,95 +44,57 @@ public class RenderingControlEventHandler extends DefaultHandler {
 
 	private final Map<RenderingControlEventHandler.RenderingControlEventType, String> changes = new HashMap<RenderingControlEventHandler.RenderingControlEventType, String>();
 
-	/** The get preset name. */
 	private boolean getPresetName = false;
 
-	/** The preset name. */
 	private String presetName;
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * @see org.xml.sax.helpers.DefaultHandler#characters(char[], int, int)
-	 */
 	@Override
-	public void characters(char[] ch, int start, int length)
-			throws SAXException {
+	public void characters(char[] ch, int start, int length) throws SAXException {
 		if (this.getPresetName) {
 			this.presetName = new String(ch, start, length);
 		}
 	}
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * @see org.xml.sax.helpers.DefaultHandler#endElement(java.lang.String,
-	 *      java.lang.String, java.lang.String)
-	 */
 	@Override
-	public void endElement(String uri, String localName, String qName)
-			throws SAXException {
+	public void endElement(String uri, String localName, String qName) throws SAXException {
 		if (this.getPresetName) {
 			this.getPresetName = false;
-			this.changes.put(RenderingControlEventType.PRESET_NAME,
-					this.presetName);
+			this.changes.put(RenderingControlEventType.PRESET_NAME, this.presetName);
 		}
 	}
 
-	/**
-	 * Gets the changes.
-	 * 
-	 * @return the changes
-	 */
 	public Map<RenderingControlEventType, String> getChanges() {
 		return this.changes;
 	}
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * @see org.xml.sax.helpers.DefaultHandler#startElement(java.lang.String,
-	 *      java.lang.String, java.lang.String, org.xml.sax.Attributes)
-	 */
 	@Override
-	public void startElement(String uri, String localName, String qName,
-			Attributes atts) throws SAXException {
+	public void startElement(String uri, String localName, String qName, Attributes atts) throws SAXException {
 		if ("Volume".equals(qName)) {
 			if ("Master".equals(atts.getValue("channel"))) {
-				this.changes.put(RenderingControlEventType.VOLUME_MASTER,
-						atts.getValue("val"));
+				this.changes.put(RenderingControlEventType.VOLUME_MASTER, atts.getValue("val"));
 			} else if ("LF".equals(atts.getValue("channel"))) {
-				this.changes.put(RenderingControlEventType.VOLUME_LF,
-						atts.getValue("val"));
+				this.changes.put(RenderingControlEventType.VOLUME_LF, atts.getValue("val"));
 			} else if ("RF".equals(atts.getValue("channel"))) {
-				this.changes.put(RenderingControlEventType.VOLUME_RF,
-						atts.getValue("val"));
+				this.changes.put(RenderingControlEventType.VOLUME_RF, atts.getValue("val"));
 			} // ignore other channels
 		} else if ("Mute".equals(qName)) {
 			if ("Master".equals(atts.getValue("channel"))) {
-				this.changes.put(RenderingControlEventType.MUTE_MASTER,
-						atts.getValue("val"));
+				this.changes.put(RenderingControlEventType.MUTE_MASTER, atts.getValue("val"));
 			} else if ("LF".equals(atts.getValue("channel"))) {
-				this.changes.put(RenderingControlEventType.MUTE_LF,
-						atts.getValue("val"));
+				this.changes.put(RenderingControlEventType.MUTE_LF, atts.getValue("val"));
 			} else if ("RF".equals(atts.getValue("channel"))) {
-				this.changes.put(RenderingControlEventType.MUTE_RF,
-						atts.getValue("val"));
+				this.changes.put(RenderingControlEventType.MUTE_RF, atts.getValue("val"));
 			} // ignore other channels
 		} else if ("Bass".equals(qName)) {
-			this.changes.put(RenderingControlEventType.BASS,
-					atts.getValue("val"));
+			this.changes.put(RenderingControlEventType.BASS, atts.getValue("val"));
 		} else if ("Treble".equals(qName)) {
-			this.changes.put(RenderingControlEventType.TREBLE,
-					atts.getValue("val"));
+			this.changes.put(RenderingControlEventType.TREBLE, atts.getValue("val"));
 		} else if ("Loudness".equals(qName)) {
 			if ("Master".equals(atts.getValue("channel"))) {
-				this.changes.put(RenderingControlEventType.LOUDNESS,
-						atts.getValue("val"));
+				this.changes.put(RenderingControlEventType.LOUDNESS, atts.getValue("val"));
 			} // ignore other channels
 		} else if ("OutputFixed".equals(qName)) {
-			this.changes.put(RenderingControlEventType.OUTPUT_FIXED,
-					atts.getValue("val"));
+			this.changes.put(RenderingControlEventType.OUTPUT_FIXED, atts.getValue("val"));
 		} else if ("PresetNameList".equals(qName)) {
 			this.getPresetName = true;
 		}
